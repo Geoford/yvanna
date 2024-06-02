3e948fc758b4556c9fa5336d08ca072cfe909725
JemLlanto <105899347+JemLlanto@users.noreply.github.com>
2024-06-02 14:55:22 +0800

    Add files via upload

---
+++ thesis.sql
@@ -0,0 +1,173 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 02, 2024 at 08:54 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `thesis`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_table`
+--
+
+CREATE TABLE `order_table` (
+  `order_id` int(15) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `price` int(10) NOT NULL,
+  `total` int(10) NOT NULL,
+  `variation` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `product_id` int(11) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `image_file` varchar(255) NOT NULL,
+  `price` int(10) NOT NULL,
+  `hot` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`product_id`, `product_name`, `image_file`, `price`, `hot`) VALUES
+(25, 'Button Pin', '6658400e2d58d.jpg', 129, 0),
+(26, 'GLASS ART', '665a670e2431d.jpg', 249, 0),
+(30, 'Phone Case', '665c1544ab66c.jpg', 359, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_samples`
+--
+
+CREATE TABLE `product_samples` (
+  `sample_id` int(15) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `image_file` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `product_samples`
+--
+
+INSERT INTO `product_samples` (`sample_id`, `product_id`, `image_file`) VALUES
+(11, 0, '665c14b20668b.jpg'),
+(12, 26, '665c150b20fdb.jpg'),
+(13, 25, '665c1521dc1c6.jpg'),
+(14, 30, '665c1561e52f8.jpg'),
+(15, 30, '665c15791347f.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_table`
+--
+
+CREATE TABLE `user_table` (
+  `user_id` int(50) NOT NULL,
+  `is_admin` tinyint(1) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `sex` varchar(50) NOT NULL,
+  `phone` varchar(50) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `province` varchar(50) NOT NULL,
+  `zip` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_table`
+--
+
+INSERT INTO `user_table` (`user_id`, `is_admin`, `first_name`, `last_name`, `sex`, `phone`, `address`, `city`, `province`, `zip`, `username`, `email`, `password`) VALUES
+(2, 1, 'John Victor', 'Silva', 'Male', '0965 145 2493', '', '', '', '', 'Jambik', 'bektor@gmail.com', '$2y$10$GC1D7RnzKxT6u1b4T12M9eDVXg4tY.X0F7gj6xJKUWY'),
+(15, 0, 'Jem', 'llanto', 'male', '0965 145 2493', '123 village', 'Tanza', 'Cavite', '4108', 'jem', 'jem@gmail.com', '$2y$10$DYymJsxM.zkv.JiJg8viJ.fHV7dN/PMKOowV.FqHdWH');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `order_table`
+--
+ALTER TABLE `order_table`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- Indexes for table `product_samples`
+--
+ALTER TABLE `product_samples`
+  ADD PRIMARY KEY (`sample_id`);
+
+--
+-- Indexes for table `user_table`
+--
+ALTER TABLE `user_table`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `order_table`
+--
+ALTER TABLE `order_table`
+  MODIFY `order_id` int(15) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT for table `product_samples`
+--
+ALTER TABLE `product_samples`
+  MODIFY `sample_id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `user_table`
+--
+ALTER TABLE `user_table`
+  MODIFY `user_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
